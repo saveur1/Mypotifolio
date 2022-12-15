87c447dc2fd96fcc01b1c2d5c08994fa3b2debe6
saveur1 <104947935+saveur1@users.noreply.github.com>
2022-12-15 16:27:13 +0200

    simple tooltip and a little varidation added

--- javascript.js
+++ javascript.js
@@ -6,7 +6,17 @@ function validateContactForm()
     let  phone=document.getElementById("phone");
     let  subject=document.getElementById("subject");
     let  message=document.getElementById("message");
+    let firstname_error=document.getElementsByClassName("firstname_error")[0];
+    let lastname_error=document.getElementsByClassName("lastname_error")[0];
     let error=0;
-    if(first_name=="")
-
+    if(first_name.value=="")
+    {
+        firstname_error.style.display="block";
+        error++;
+    }
+    if(last_name.value=="")
+    {
+        lastname_error.style.display="block";
+        error++;
+    }
 }
