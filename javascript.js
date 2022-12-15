309579a39e715ceb63e8eb9b1495734cebd5887e
saveur1 <104947935+saveur1@users.noreply.github.com>
2022-12-15 12:27:15 +0200

    changes

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function validateContactForm()
+{
+    let  first_name=document.getElementById("first_name");
+    let  last_name=document.getElementById("last_name");
+    let  useremailaddr=document.getElementById("emailaddr");
+    let  phone=document.getElementById("phone");
+    let  subject=document.getElementById("subject");
+    let  message=document.getElementById("message");
+    let error=0;
+    if(first_name=="")
+
+}
